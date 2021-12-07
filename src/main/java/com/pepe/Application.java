e3caa6577e46d143f3591fe39d276564772a35ac
PepeAle <jose.alegre@infinant.pe>
2021-12-07 01:50:18 -0500

    Lesson 16. Logs

--- src/main/java/com/pepe/Application.java
+++ src/main/java/com/pepe/Application.java
@@ -1,9 +1,12 @@
 package com.pepe;
 
+import com.pepe.bean.IBean3WithDependency;
 import com.pepe.bean.IPersonBean;
 import com.pepe.bean.PropertiesBean;
 import com.pepe.pojo.Person;
 import com.pepe.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,18 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+	Log LOGGER = LogFactory.getLog(Application.class);
+
 	// Inyección de dependencias
 
 //	private IComponentDependency componentDependency;
 //	private IBean1 bean;
-//	private IBean3WithDependency bean3WithDependency;
+	private IBean3WithDependency bean3WithDependency;
 //	private IPersonBean personBean;
 //	private PropertiesBean propertiesBean;
-	private UserPojo userPojo;
-
-	public Application(UserPojo userPojo) {
-		this.userPojo = userPojo;
-	}
+//	private UserPojo userPojo;
+//
+//	public Application(UserPojo userPojo) {
+//		this.userPojo = userPojo;
+//	}
 	//
 //	public Application(PropertiesBean propertiesBean) {
 //		this.propertiesBean = propertiesBean;
@@ -39,9 +44,9 @@ public class Application implements CommandLineRunner {
 //		this.bean = bean;
 //	}
 
-//	public Application(IBean3WithDependency bean3WithDependency) {
-//		this.bean3WithDependency = bean3WithDependency;
-//	}
+	public Application(IBean3WithDependency bean3WithDependency) {
+		this.bean3WithDependency = bean3WithDependency;
+	}
 
 //	public Application(IPersonBean personBean) {
 //		this.personBean = personBean;
@@ -51,12 +56,19 @@ public class Application implements CommandLineRunner {
 	public void run(String... args) {
 //		componentDependency.saludar();
 //		bean.print();
-//		bean3WithDependency.printWithDependency();
+		bean3WithDependency.printWithDependency();
 //		bean3WithDependency.displayElements();
 //		Other example
 //		personBean.greet(new Person("Pepe",43));
 //		System.out.println(propertiesBean.function());
-		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+//		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+		try {
+			//error
+			int value = 10/0;
+			LOGGER.debug("I never will here");
+		} catch (Exception e){
+			LOGGER.error("It is an error. "+e.getMessage()+" "+e.toString());
+		}
 
 	}
 
--- src/main/java/com/pepe/implement/Bean3WithDependencyImplement.java
+++ src/main/java/com/pepe/implement/Bean3WithDependencyImplement.java
@@ -2,10 +2,15 @@ package com.pepe.implement;
 
 import com.pepe.bean.IBean2;
 import com.pepe.bean.IBean3WithDependency;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.List;
 
 public class Bean3WithDependencyImplement implements IBean3WithDependency {
+
+    Log LOGGER = LogFactory.getLog(Bean3WithDependencyImplement.class);
+
 
     private IBean2 myOperation;
 
@@ -15,9 +20,12 @@ public class Bean3WithDependencyImplement implements IBean3WithDependency {
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("I am in the printWithDependency method");
         int age=43;
+        LOGGER.debug("the age is "+ age);
         System.out.println("I am "+myOperation.sum(age));
         System.out.println("Hi Pp. I am the implementation of your bean with dependency.");
+
     }
 
     @Override
